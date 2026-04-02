c2736eb8a9a59abe69a8653a828bc96ac0c66606
Muddasir Nisar <mnisar@teresol.com>
2026-04-02 12:13:02 +0500

    Updated MediaPipe Library Changes.

--- src/detection/pose_detector.py
+++ src/detection/pose_detector.py
@@ -1,20 +1,41 @@
-import mediapipe as mp
 import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from src.config import MODEL_PATH
 
 class PoseDetector:
-    def __init__(self):
-        self.mp_pose = mp.solutions.pose
-        self.pose = self.mp_pose.Pose()
-        self.mp_draw = mp.solutions.drawing_utils
+    def __init__(self, model_path=MODEL_PATH):
+        BaseOptions = python.BaseOptions
+        PoseLandmarker = vision.PoseLandmarker
+        PoseLandmarkerOptions = vision.PoseLandmarkerOptions
+        VisionRunningMode = vision.RunningMode
 
-    def process(self, frame):
+        options = PoseLandmarkerOptions(
+            base_options=BaseOptions(model_asset_path=model_path),
+            running_mode=VisionRunningMode.VIDEO,
+            num_poses=1
+        )
+
+        self.detector = PoseLandmarker.create_from_options(options)
+
+    # returns: result.pose_landmarks → list of detected body joints
+    def process(self, frame, timestamp_ms):
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        return self.pose.process(rgb)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
+
+        result = self.detector.detect_for_video(mp_image, timestamp_ms)
+        return result
 
     def draw(self, frame, result):
-        if result.pose_landmarks:
-            self.mp_draw.draw_landmarks(
-                frame,
-                result.pose_landmarks,
-                self.mp_pose.POSE_CONNECTIONS
-            )
+        if not result.pose_landmarks:
+            return frame
+
+        for pose_landmarks in result.pose_landmarks:
+            for landmark in pose_landmarks:
+                h, w, _ = frame.shape
+                x = int(landmark.x * w)
+                y = int(landmark.y * h)
+                cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
+
+        return frame
